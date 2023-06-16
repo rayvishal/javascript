a3d83df48954042c7453e20b007ec7fc183e5823
rayvishal <vishal76620@gmail.comm>
2023-06-16 12:40:20 +0530

    Initial commit

---
+++ 2.convert_minutes_into_seconds.js
@@ -0,0 +1,5 @@
+function convert(minutes) {
+    const seconds = minutes * 60;
+    console.log(seconds);
+}
+convert(6)
---
+++ 6.return_the_first_element_in_an_array.js
@@ -0,0 +1,4 @@
+function fstelement(arrayele) {
+    return (arrayele[0])
+}
+fstelement([3, 5])
