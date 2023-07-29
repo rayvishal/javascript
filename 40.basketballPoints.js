1bb26cc16798aa59c11965e2a458c6256210246f
rayvishal <vishal76620@gmail.comm>
2023-07-29 23:15:51 +0530

    Add five program of js

---
+++ 40.basketballPoints.js
@@ -0,0 +1,5 @@
+// You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+function points(twoPointers, threePointers) {
+    console.log(2 * twoPointers + 3 * threePointers)
+};
+points(7, 5);
---
+++ 41.basicVariableAssignment.js
@@ -0,0 +1,7 @@
+// A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+function namestring(name) {
+    var b = "Edabit";
+    var result = name + b;
+    result(result)
+}
+namestring("vishal")
