f13392e75914016d44924338ea851ae6b6a07343
rayvishal <vishal76620@gmail.comm>
2023-08-21 18:53:28 +0530

    add five js program

---
+++ 67.evaluateAnEquation.js
@@ -0,0 +1,8 @@
+// Create a function that evaluates an equation
+function evaluate(eqa) {
+
+
+    console.log(eval(eqa))
+
+}
+evaluate("1+1")
---
+++ 68.dropTheFirstElementsOfAnArray.js
@@ -0,0 +1,5 @@
+function drop(arr, num) {
+    arr.splice(0, num)
+    console.log(arr)
+}
+drop([1, 2, 3], 2)
